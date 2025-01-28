145c8abad83350263d94676be3ef08941d90b863
Pariya.Zarei <art0pariya0z@gmail.com>
2025-01-28 18:04:06 +0330

    Update models.py

--- tasks/models.py
+++ tasks/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class task(models.Model):  
     Title = models.CharField(max_length=255 )
-    Description = models.TextField(lank=True, null=True)
+    Description = models.TextField(blank=True, null=True)
     Due_date = models.DateTimeField()
     Completed = models.BooleanField(default=False)
     Created_at = models.DateTimeField(auto_now_add=True)
